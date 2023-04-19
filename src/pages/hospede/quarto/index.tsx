0ce0772d1975d9e39ac89f47aa44d8353660df18
Yago Henner <yagohenner@gmail.com>
2023-04-19 12:31:30 -0300

    feat: dados em usuario/quarto

--- src/pages/hospede/quarto/index.tsx
+++ src/pages/hospede/quarto/index.tsx
@@ -4,6 +4,10 @@ import PageTemplate from "../../../components/PageTemplate";
 import { Quarto } from "../../../types";
 import { useLocation } from "react-router-dom";
 
+const barChildren = () => {
+  return <button>bunda</button>;
+};
+
 export default function QuartoPage() {
   const location = useLocation();
   const quartoData = location.state as Quarto;
@@ -14,13 +18,51 @@ export default function QuartoPage() {
         <div className={styles.fotoDiv}>
           <img src={quartoData.pic}></img>
         </div>
-        <div className='flex-row'>
+        <div className='flex-row' style={{ maxWidth: "90%" }}>
           <div className={styles.section}>
             <div className={styles.sectionTitle}>
               Quarto {`${quartoData.numero}`}
             </div>
             <div className={styles.sectionDescription}>
               {quartoData.idTipoQuarto.descricao}
+            </div>
+          </div>
+          <div className={styles.section}>
+            <div className={styles.dadosQuarto}>
+              <div className={styles.cardDadosQuarto}>
+                <div className={styles.tituloDado}>Capacidade</div>
+                <div className={styles.qtdDado}>
+                  {quartoData.idTipoQuarto.capacidade}
+                </div>
+              </div>
+              <div className={styles.cardDadosQuarto}>
+                <div className={styles.tituloDado}>Camas de Casal</div>
+                <div className={styles.qtdDado}>
+                  {quartoData.idTipoQuarto.camaCasal}
+                </div>
+              </div>
+              <div className={styles.cardDadosQuarto}>
+                <div className={styles.tituloDado}>Camas de Solteiro</div>
+                <div className={styles.qtdDado}>
+                  {quartoData.idTipoQuarto.camaSolteiro}
+                </div>
+              </div>
+              <div className={styles.cardDadosReserva}>
+                <div className={styles.tituloDado}>Dados da Reserva</div>
+                <div className={styles.reservaDescription}>
+                  <div className={styles.dataEscolhida}>
+                    <span>Data escolhida:</span>
+                    03/08 até 10/08
+                  </div>
+                  <div className={styles.qtdDiaria}>
+                    <span>Diária:</span>
+                    R${quartoData.idTipoQuarto.diaria}
+                  </div>
+                </div>
+                <button className={styles.buttonReserva}>
+                  Solicitar Reserva
+                </button>
+              </div>
             </div>
           </div>
         </div>
