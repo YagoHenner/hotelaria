140d9cf2ec4e30e901bd438b925099a86200c13e
Yago Henner <yagohenner@gmail.com>
2023-04-19 14:54:23 -0300

    feat: filtros + tiposquarto

---
+++ src/data/TiposQuarto.tsx
@@ -0,0 +1,29 @@
+export const TiposQuarto = [
+  {
+    id: 0,
+    nome: "Casal",
+    descricao: "Para um casal",
+    capacidade: 2,
+    camaCasal: 1,
+    camaSolteiro: 0,
+    diaria: 250,
+  },
+  {
+    id: 1,
+    nome: "Solteiro",
+    descricao: "Quarto de Solteiro",
+    capacidade: 1,
+    camaCasal: 0,
+    camaSolteiro: 1,
+    diaria: 120,
+  },
+  {
+    id: 2,
+    nome: "Familia",
+    descricao: "Para Familia",
+    capacidade: 4,
+    camaCasal: 1,
+    camaSolteiro: 2,
+    diaria: 350,
+  },
+];
--- src/pages/hospede/homehospede/index.tsx
+++ src/pages/hospede/homehospede/index.tsx
@@ -4,11 +4,66 @@ import MenuLateral from "../../../components/MenuLateral";
 import { Quartos } from "../../../data/Quartos";
 import styles from "./HomeUsuario.module.css";
 import PageTemplate from "../../../components/PageTemplate";
+import { useCallback, useEffect, useState } from "react";
+import { TiposQuarto } from "../../../data/TiposQuarto";
 
 export default function HomeHospede() {
+  const getInitialStartDate = useCallback(() => {
+    const date = new Date();
+    return date;
+  }, []);
+
+  const getInitialEndDate = useCallback(() => {
+    const date = new Date();
+    date.setDate(date.getDate() + 7);
+    return date;
+  }, []);
+
+  const [startDate, setStartDate] = useState(getInitialStartDate());
+  const [endDate, setEndDate] = useState(getInitialEndDate());
+  const [tipoFiltro, setTipoFiltro] = useState("");
+
+  useEffect(() => {
+    setStartDate(getInitialStartDate());
+    setEndDate(getInitialEndDate());
+  }, [getInitialStartDate, getInitialEndDate]);
+
   return (
     <div>
       <PageTemplate title={"Quartos Disponíveis"}>
+        <div className={styles.filtros}>
+          De:{" "}
+          <input
+            type='date'
+            value={startDate?.toISOString().slice(0, 10)}
+            onChange={(event: any) =>
+              setStartDate(new Date(event.target.value))
+            }
+          ></input>
+          Até:{" "}
+          <input
+            type='date'
+            value={endDate?.toISOString().slice(0, 10)}
+            onChange={(event: any) =>
+              setStartDate(new Date(event.target.value))
+            }
+          ></input>
+          Tipo:{" "}
+          <select
+            value={tipoFiltro}
+            onChange={(event) => setTipoFiltro(event.target.value)}
+          >
+            {TiposQuarto &&
+              TiposQuarto.map((item) => {
+                return (
+                  <option key={item.id} value={item.nome}>
+                    {item.nome}
+                  </option>
+                );
+              })}
+          </select>
+        </div>
+
         <div className={styles.divCards}>
           {Quartos &&
             Quartos.map((quarto) => {
