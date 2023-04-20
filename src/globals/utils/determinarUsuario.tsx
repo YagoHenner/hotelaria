005e0cf6351aee4ba35eb15ad54bee10bbd405e3
Yago Henner <yagohenner@gmail.com>
2023-04-20 17:43:53 -0300

    ler: determinar usuario

--- src/globals/utils/determinarUsuario.tsx
+++ src/globals/utils/determinarUsuario.tsx
@@ -24,3 +24,75 @@ export default function determinarUsuario(email: string, senha: string) {
 
   return { tipo: tipo, user: user };
 }
+
+{
+  /*interface ComidaContextType {
+  comida: Comida | null;
+  setComida: React.Dispatch<React.SetStateAction<Comida | null>>;
+}
+Crie um provedor de contexto que inicialize o estado com o valor do localStorage e forneça as funções setComida e comida:
+typescript
+Copy code
+import React, { useState } from "react";
+
+const ComidaContext = React.createContext<ComidaContextType>({
+  comida: null,
+  setComida: () => {},
+});
+
+const ComidaProvider: React.FC = ({ children }) => {
+  const [comida, setComida] = useState<Comida | null>(
+    JSON.parse(localStorage.getItem("comida") || "null")
+  );
+
+  return (
+    <ComidaContext.Provider value={{ comida, setComida }}>
+      {children}
+    </ComidaContext.Provider>
+  );
+};
+Em seguida, crie um componente que use o provedor de contexto e implemente a lógica para encontrar a comida e armazenar o id no localStorage:
+typescript
+Copy code
+import React, { useContext, useState } from "react";
+
+const ComidaFinder = () => {
+  const { setComida } = useContext(ComidaContext);
+  const [nomeComida, setNomeComida] = useState("");
+
+  const procurarComida = () => {
+    const comidaEncontrada = comidas.find(
+      (comida) => comida.nome === nomeComida
+    );
+
+    if (comidaEncontrada) {
+      localStorage.setItem("comida", comidaEncontrada.id.toString());
+      setComida(comidaEncontrada);
+    }
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        value={nomeComida}
+        onChange={(e) => setNomeComida(e.target.value)}
+      />
+      <button onClick={procurarComida}>Procurar</button>
+    </>
+  );
+};
+Para usar o provedor de contexto em um componente filho, você pode simplesmente importá-lo e envolver o componente em torno do componente filho:
+typescript
+Copy code
+import { ComidaProvider } from "./ComidaContext";
+
+const App = () => {
+  return (
+    <ComidaProvider>
+      <ComidaFinder />
+    </ComidaProvider>
+  );
+};
+Dessa forma, você pode armazenar o estado da comida em um contexto com o valor padrão do localStorage e permitir que outros componentes acessem e atualizem esse estado.*/
+}
